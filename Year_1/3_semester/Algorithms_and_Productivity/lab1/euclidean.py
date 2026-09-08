267d2d55e9aa7cdad018a3659ac460e003d2ec89
Aroowa_Malikov <ilyasolovev0207@gmail.com>
2026-09-08 14:21:59 +0300

    modified:   Year_1/3_semester/Algorithms_and_Productivity/lab1/euclidean.py

--- Year_1/3_semester/Algorithms_and_Productivity/lab1/euclidean.py
+++ Year_1/3_semester/Algorithms_and_Productivity/lab1/euclidean.py
@@ -1,4 +1,5 @@
 import time
+from random import randint
 
 M = int(input('First input Num: '))
 N = int(input('Second input Num: '))
@@ -14,12 +15,12 @@ def gcd_enum(M, N):
             gcd = num
         num += 1
     milestone2 = time.time()
-    return (gcd ,milestone2 - milestone1)
+    return (gcd ,milestone2 - milestone1, counter)
 
 
 def gcd_rev_enum(M, N):
     milestone1 = time.time()
-    gcd, num = 1, 
+    gcd, num = 1, 1
     counter = 0
     while num > 0:
         counter += 1
@@ -27,4 +28,30 @@ def gcd_rev_enum(M, N):
             gcd = num
         num -= 1
     milestone2 = time.time()
-    return (gcd ,milestone2 - milestone1)
+    return (gcd ,milestone2 - milestone1, counter)
+
+
+def gcd_euclidean(M, N):
+    milestone1 = time.time()
+    counter = 0
+    if N >= M:
+        counter += 1
+        if M == 0:
+            gcd = N
+        gcd = gcd_euclidean(N % M, M)
+    else:
+        counter += 1
+        if N == 0:
+            gcd = M
+        gcd = gcd_euclidean(N, M % N)
+    milestone2 = time.time()
+    return (gcd, milestone1 - milestone2, counter)
+
+"""Research part"""
+
+exp = int(input('Enter the amount of experiments: '))
+
+u_border = 10 ** 5 #upper border of a random number
+for _ in range(exp):
+    N, M = randint(1, u_border), randint(1, u_border)
+    
