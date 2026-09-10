e6839b5eba51dbe4152b8d94362de875d70f4280
Aroowa_Malikov <ilyasolovev0207@gmail.com>
2026-09-10 13:37:02 +0300

    modified:   Year_1/3_semester/Algorithms_and_Productivity/lab_2/heapsort.py

--- Year_1/3_semester/Algorithms_and_Productivity/lab_2/heapsort.py
+++ Year_1/3_semester/Algorithms_and_Productivity/lab_2/heapsort.py
@@ -3,16 +3,23 @@ def heapsort(arr):
     Пирамидальная сортировка.
     """
     n = len(arr)
+    comparisons = 0
 
+    # Построение кучи
     for i in range(n // 2 - 1, -1, -1):
         while True:
                 largest = i
                 l = 2 * i + 1
                 r = 2 * i + 2
 
+                # Сравнение левого потомка
                 if l < n and arr[l] > arr[largest]:
+                    comparisons += 1
                     largest = l
+                
+                # Сравнение правого потомка
                 if r < n and arr[r] > arr[largest]:
+                    comparisons += 1
                     largest = r
 
                 if largest == i:
@@ -21,6 +28,7 @@ def heapsort(arr):
                 arr[i], arr[largest] = arr[largest], arr[i]
                 i = largest
 
+    # Сортировка (извлечение элементов)
     for i in range(n - 1, 0, -1):
         arr[0], arr[i] = arr[i], arr[0]
         i = 0
@@ -30,9 +38,14 @@ def heapsort(arr):
                 l = 2 * i + 1
                 r = 2 * i + 2
 
+                # Сравнение левого потомка
                 if l < n and arr[l] > arr[largest]:
+                    comparisons += 1
                     largest = l
+                
+                # Сравнение правого потомка
                 if r < n and arr[r] > arr[largest]:
+                    comparisons += 1
                     largest = r
 
                 if largest == i:
@@ -41,4 +54,4 @@ def heapsort(arr):
                 arr[i], arr[largest] = arr[largest], arr[i]
                 i = largest
 
-    return arr
+    return arr, comparisons
